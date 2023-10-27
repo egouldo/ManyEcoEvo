5424771d1e2aaf0a2559a158d1cb4f992646e52d
Elliot Gould <elliot.gould@unimelb.edu.au>
2023-10-27 19:05:35 +1100

    Add pipeline output to data/ and add script to call tar_make() and use_data()

---
+++ data-raw/tar_make.R
@@ -0,0 +1,29 @@
+# ---- Load Libraries ----
+library(tidyverse)
+library(targets)
+library(usethis)
+
+# ---- Run targets pipeline ----
+# tar_destroy() # Uncomment and run if want to reproduce from scratch, run locally only
+tar_make()
+
+# ---- Read targets and use data ----
+
+# Targets required for main manuscript:
+tar_load(ManyEcoEvo)
+tar_load(ManyEcoEvo_yi)
+tar_load(ManyEcoEvo_results)
+tar_load(ManyEcoEvo_yi_results)
+tar_load(ManyEcoEvo_viz)
+tar_load(ManyEcoEvo_yi_viz)
+
+usethis::use_data(ManyEcoEvo, 
+                  ManyEcoEvo_yi,
+                  ManyEcoEvo_results,
+                  ManyEcoEvo_yi_results,
+                  ManyEcoEvo_viz,
+                  ManyEcoEvo_yi_viz,
+                  overwrite = TRUE)
+
+# TODO also need to add raw files to demonstrate pipeline / package functionality in package vignette and software manuscript.
+
