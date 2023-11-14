a51c6bbb5fa2c1971c55744319198738a7164107
Elliot Gould <elliot.gould@unimelb.edu.au>
2023-11-14 12:12:42 +1100

    #24 add summary functions for table 3.1 qualitative conclusions

---
+++ R/summarise_conclusions.R
@@ -0,0 +1,78 @@
+summarise_conclusions <- function(variables) {
+  effect_ids <- ManyEcoEvo_results %>% 
+    filter(exclusion_set == "complete", 
+           publishable_subset == "All") %>% 
+    ungroup %>% 
+    select(MA_mod, effects_analysis, estimate_type, dataset) %>% 
+    group_by(estimate_type, dataset) %>% 
+    mutate(tidy_mod = map(MA_mod, 
+                          ~ broom::tidy(.x, 
+                                        conf.int = TRUE, 
+                                        include_studies = TRUE) %>% 
+                            rename(study_id = term)), .keep = "none") %>% 
+    unnest(tidy_mod) %>% 
+    filter(type =="study") %>% 
+    ungroup %>% 
+    select(study_id) %>% 
+    rename(id_col = study_id) %>% #TODO duplicates for "Bell-2-2-1" and "Bonalbo-1-1-1 WHY?
+    distinct()
+  
+  prediction_ids <- ManyEcoEvo_yi_results %>% #TODO Euc mod_data_logged not here!
+    filter(exclusion_set == "complete", 
+           # dataset == "blue tit"
+    ) %>% 
+    ungroup %>% 
+    select(MA_mod, effects_analysis, -exclusion_set, dataset, estimate_type) %>% 
+    group_by(estimate_type, dataset) %>% 
+    mutate(tidy_mod = map(MA_mod, 
+                          ~ broom::tidy(.x, conf.int = TRUE, include_studies = TRUE) %>% 
+                            rename(study_id = term)), .keep = "none") %>% 
+    unnest(tidy_mod) %>% filter(type == "study") %>% 
+    ungroup %>% 
+    select(study_id) %>% 
+    rename(id_col = study_id) %>% 
+    distinct()
+  
+  Master <-
+    ManyEcoEvo %>% 
+    ungroup %>% 
+    select(data) %>% 
+    unnest(data) %>% 
+    mutate(across(c(num_fixed_variables,
+                    num_random_variables,
+                    sample_size,
+                    num_interactions,
+                    Bayesian,
+                    mixed_model), 
+                  as.numeric),
+           lm = ifelse(linear_model == "linear", 1, 0), 
+           glm = ifelse(linear_model == "generalised", 1, 0)) %>%  #TODO move this into master processing so don't have to repeat else where!!
+    filter(Conclusion != "CHECK", !is.na(Conclusion)) #TODO data cleaning, check these
+  
+  effects <- Master %>% 
+    right_join(effect_ids, by = c("id_col")) # repeat for each
+  
+  predictions <- Master %>% 
+    right_join(prediction_ids, by = c("id_col"))
+  
+  summarised_data <- 
+    map_dfr(.x = list(effects, predictions, Master) %>% 
+              purrr::set_names("effects", "predictions", "all"),
+            .f = summarise_conclusions_data,
+            .id = "subset") %>% 
+    pivot_wider(names_from = Conclusion, 
+                values_from = n,
+                values_fill = 0)
+  
+  return(summarised_data)
+  
+}
+
+
+summarise_conclusions_data <- function(data){
+  data %>% 
+    ungroup %>% 
+    filter(split_id == "1", analysis_id == "1" ) %>% #TODO how to generalise to data without split_id
+    group_by(dataset, Conclusion) %>% 
+    count()
+}
