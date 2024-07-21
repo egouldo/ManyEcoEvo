c642b38813b3b9dfdf4051266f776e0c624a0404
egouldo <egouldo@users.noreply.github.com>
2024-07-22 00:47:59 +1000

    in box_cox_transform function, do not execute fold_mu calculation twice - merge folded_v and folded_mu functions into single function that returns a list - update downstream execution of box-cox variance computation

--- R/box_cox_transform.R
+++ R/box_cox_transform.R
@@ -43,33 +43,23 @@ box_cox_transform <- function(data, dataset) {
         return(variance_bc)
       }
       
-      # folded abs_dev_score
-      folded_mu <- function(abs_dev_score, VZr) {
+      folded_params <- function(abs_dev_score, VZr){
         mu <- abs_dev_score
         sigma <- sqrt(VZr)
-        fold_mu <- sigma * sqrt(2/pi) * exp((-mu^2)/(2 * sigma^2)) + mu * (1 - 2 * pnorm(-mu/sigma))
-        fold_mu
-      }
-      
-      # folded VZr
-      folded_v <- function(abs_dev_score, VZr) {
-        mu <- abs_dev_score
-        sigma <- sqrt(VZr)
-        fold_mu <- sigma * sqrt(2/pi) * exp((-mu^2)/(2 * sigma^2)) + mu * (1 - 2 * pnorm(-mu/sigma))
+        fold_mu <- sigma * sqrt(2/pi) * exp((-mu^2)/(2 * sigma^2)) + mu * (1 - 2 * pnorm(-mu/sigma)) # folded abs_dev_score
         fold_se <- sqrt(mu^2 + sigma^2 - fold_mu^2)
-        # adding se to make bigger abs_dev_score
-        fold_v <- fold_se^2
-        fold_v
+        fold_v <- fold_se^2 # folded VZr
+        return(list(fold_mu = fold_mu, fold_v = fold_v))
       }
       
-      Z_colname <- data %>% colnames %>% keep(., str_starts(., "Z"))
+      # Z_colname <- data %>% colnames %>% keep(., str_starts(., "Z"))
       VZ_colname <- data %>% colnames %>% keep(., str_starts(., "VZ"))
       result <- recipes::juice(box_cox_recipe) %>% 
         rename_with(.fn = ~ paste0("box_cox_", .x)) %>% 
         bind_cols(data, .) %>% 
-        mutate(folded_mu_val = folded_mu(abs_deviation_score_estimate, !!as.name(VZ_colname)),
-               folded_v_val = folded_v(abs_deviation_score_estimate, !!as.name(VZ_colname)),
-               box_cox_var = variance_box_cox(folded_mu_val, folded_v_val, lambda[[1]]),
+        mutate(fold_params = folded_params(abs_deviation_score_estimate, !!as.name(VZ_colname))) %>% 
+        hoist(fold_params, folded_mu_val = 1, folded_v_val = 2 ) %>% 
+        mutate(box_cox_var = variance_box_cox(folded_mu_val, folded_v_val, lambda[[1]]),
                lambda = lambda[[1]])
       
     } else{
