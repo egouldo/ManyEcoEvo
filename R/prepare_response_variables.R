c58621dc4dacbd3b51c7b68a80e37da3dd5e0996
egouldo <egouldo@users.noreply.github.com>
2024-08-14 13:28:30 +1000

    #102 add `@imports` call and document new param for new arg in `prepare_response_variables()` #88

--- R/prepare_response_variables.R
+++ R/prepare_response_variables.R
@@ -3,12 +3,16 @@
 #' @param ManyEcoEvo Complete ManyEcoEvo dataset containing nested datasets for each different analysis and exclusion set dataset
 #' @param estimate_type A character string of length 1, equal to either "Zr", "yi", "y25", "y50", "y75", indicating what type of estimates are being prepared.
 #' @param param_table A table of parameters \(mean, sd\) for *most* response variables used by analysts. This tibble is pulled from the named object exported by `ManyEcoEvo::`. but can be overwritten with the users's own `param_table` dataset.
-#'
+#' @param dataset_standardise A character string of length 1, equal to the name of the dataset to standardise the response variables to. If `NULL`, all datasets are standardised.
 #' @return A tibble of nested list-columns
 #' @details Operates on nested list-columns of dataframes, where each dataframe contains the response variable data for a single analysis. The function standardises the response variable data for each analysis, and returns the modified dataset to the `data` list-column.
 #' @family targets-pipeline functions.
 #' @family Multi-dataset Wrapper Functions
 #' @export
+#' @import cli
+#' @import dplyr
+#' @import purrr
+#' @import rlang
 prepare_response_variables <- function(ManyEcoEvo,
                                        estimate_type = character(1L),
                                        param_table = NULL,
