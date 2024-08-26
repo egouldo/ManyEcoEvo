bd04529c3371da9bc593483a09cd3ad7135edcac
egouldo <egouldo@users.noreply.github.com>
2024-08-26 15:31:28 +1000

    bug: don't use reserved data pronoun as argument `.data`

--- R/apply_sorensen_calc.R
+++ R/apply_sorensen_calc.R
@@ -1,6 +1,6 @@
 #' Applies the sorensen diversity index calculation to variable diversity dataset
 #'
-#' @param .data A variable diversity dataset with columns for each unique variable used in all analyses. Values are `NA` if the analysis did not use that variable, and take the value of the variable as a character string if the analysis did use that variable. Each unique analysis is a row.
+#' @param data A variable diversity dataset with columns for each unique variable used in all analyses. Values are `NA` if the analysis did not use that variable, and take the value of the variable as a character string if the analysis did use that variable. Each unique analysis is a row.
 #' 
 #' @seealso [calculate_sorensen_diversity_index()]
 #'
@@ -8,12 +8,12 @@
 #' @export
 #' @import dplyr
 #' @family Analysis-level functions
-apply_sorensen_calc <- function(.data) {
+apply_sorensen_calc <- function(data) {
 
-  out <- .data %>%
+  out <- data %>%
     select(-dataset) %>% # TODO THEN REMOVE DUPES
     filter(id_col %nin% {
-      out %>%
+      data %>%
         count(id_col) %>%
         filter(n > 1) %>%
         pull(id_col)
