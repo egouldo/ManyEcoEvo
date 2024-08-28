2a4b01d33edc65411f92bdd80179e749acdce25d
Elliot Gould <egouldo@users.noreply.github.com>
2024-08-29 02:17:06 +1000

    Update R/box_cox_transform.R

--- R/box_cox_transform.R
+++ R/box_cox_transform.R
@@ -81,7 +81,7 @@ box_cox_transform <- function(data, dataset, outcome_SE_colname) {
               folded_v_val = 2) %>%
         mutate(
           box_cox_var = variance_box_cox(folded_mu_val,
-                                         folded_v_val, 
+                                         folded_v_val,
                                          lambda[[1]]),
           lambda = lambda[[1]]
         )
