4fb2dc425db0601951867da458c6fe4e7738159e
egouldo <egouldo@users.noreply.github.com>
2024-08-29 01:32:32 +1000

    build!: BREAKING CHANGES `targets::tar_make()`

--- data-raw/tar_make.R
+++ data-raw/tar_make.R
@@ -6,11 +6,11 @@ library(crew)
 library(tictoc)
 
 # ---- Run targets pipeline ----
-# tar_destroy() # Uncomment and run if want to reproduce from scratch, run locally only
+tar_destroy() # Uncomment and run if want to reproduce from scratch, run locally only
 tic()
-# tar_make()
-# tar_make(names = ManyEcoEvo_results)
-tar_make(shortcut = T, callr_function = NULL) #`callr_function` is set to NULL for debugging
+tar_make()
+# tar_make(names = ManyEcoEvo_yi_results)
+# tar_make(shortcut = T, callr_function = NULL) #`callr_function` is set to NULL for debugging
 toc()
 
 
@@ -30,11 +30,11 @@ toc()
 # Write targets to data folder:
 tic()
 usethis::use_data(ManyEcoEvo,
-  ManyEcoEvo_yi, # consider making internal
+  ManyEcoEvo_yi, #TODO consider making internal
   ManyEcoEvo_results,
-  ManyEcoEvo_yi_results, # consider making internal
+  ManyEcoEvo_yi_results, #TODO consider making internal
   ManyEcoEvo_viz,
-  ManyEcoEvo_yi_viz, # TODO consider making internal
+  ManyEcoEvo_yi_viz, #TODO consider making internal
   ManyEcoEvo_study_summary,
   overwrite = TRUE,
   compress = "gzip"
