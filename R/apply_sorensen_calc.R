c963593d7e173e86cb7c02ac4859e0e0c7c6b64e
egouldo <egouldo@users.noreply.github.com>
2025-10-16 10:14:50 +1100

    Capitalise Sorensen

--- R/apply_sorensen_calc.R
+++ R/apply_sorensen_calc.R
@@ -1,7 +1,6 @@
-#' Applies the sorensen diversity index calculation to variable diversity dataset
+#' Applies the Sorensen diversity index calculation to variable diversity dataset
 #'
 #' @param data A variable diversity dataset with columns for each unique variable used in all analyses. Values are `NA` if the analysis did not use that variable, and take the value of the variable as a character string if the analysis did use that variable. Each unique analysis is a row.
-#' 
 #' @seealso [calculate_sorensen_diversity_index()]
 #'
 #' @return A tibble containing the variables `id_col`, `mean_diversity_index` and `num_variables`
